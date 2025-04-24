6075561bb647bdc2ca8314d063902993b7c1318e
Trong Ngo <trong.ngotrung@gmail.com>
2025-04-24 22:34:16 +0700

    chore: set up eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,77 @@
 import nx from '@nx/eslint-plugin';
 
 export default [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
+    ...nx.configs['flat/base'],
+    ...nx.configs['flat/typescript'],
+    ...nx.configs['flat/javascript'],
+    {
+        ignores: ['**/dist'],
     },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
-  },
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        rules: {
+            '@nx/enforce-module-boundaries': [
+                'error',
+                {
+                    enforceBuildableLibDependency: true,
+                    allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+                    depConstraints: [
+                        {
+                            sourceTag: 'type:feature',
+                            onlyDependOnLibsWithTags: [
+                                'type:feature',
+                                'type:ui',
+                                'type:util',
+                            ],
+                        },
+                        {
+                            sourceTag: 'type:ui',
+                            onlyDependOnLibsWithTags: ['type:ui', 'type:util'],
+                        },
+                        {
+                            sourceTag: 'type:util',
+                            onlyDependOnLibsWithTags: ['type:util'],
+                        },
+                        {
+                            sourceTag: 'scope:orders',
+                            onlyDependOnLibsWithTags: [
+                                'scope:orders',
+                                'scope:products',
+                                'scope:shared',
+                            ],
+                        },
+                        {
+                            sourceTag: 'scope:products',
+                            onlyDependOnLibsWithTags: [
+                                'scope:products',
+                                'scope:shared',
+                            ],
+                        },
+                        {
+                            sourceTag: 'scope:shared',
+                            onlyDependOnLibsWithTags: ['scope:shared'],
+                        },
+                        {
+                            sourceTag: '*',
+                            onlyDependOnLibsWithTags: ['*'],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        files: [
+            '**/*.ts',
+            '**/*.tsx',
+            '**/*.cts',
+            '**/*.mts',
+            '**/*.js',
+            '**/*.jsx',
+            '**/*.cjs',
+            '**/*.mjs',
+        ],
+        // Override or add rules here
+        rules: {},
+    },
 ];
