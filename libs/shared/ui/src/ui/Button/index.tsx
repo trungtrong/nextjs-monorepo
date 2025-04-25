f23a5970ba827250e1d43d53bbb2fd98bceee80a
Trong Ngo <trong.ngotrung@gmail.com>
2025-04-25 11:58:18 +0700

    chore: init libs/shared/ui

---
+++ libs/shared/ui/src/ui/Button/index.tsx
@@ -0,0 +1,39 @@
+import type { MouseEvent } from 'react';
+import { forwardRef } from 'react';
+
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { ICommonProps } from './../../../../core/src/models';
+
+export interface ButtonProps extends ICommonProps {
+    disabled?: boolean;
+    title?: string;
+    onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+    const { children, className, disabled = false, title, ...rest } = props;
+    //
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        const { onClick } = props;
+        if (disabled) {
+            e.preventDefault();
+            return;
+        }
+        onClick?.(e);
+    };
+
+    return (
+        <button
+            ref={ref}
+            className={className}
+            title={title ?? ''}
+            data-disabled={disabled}
+            onClick={handleClick}
+            {...rest}
+        >
+          {children}
+        </button>
+    );
+});
+
+export default Button;
