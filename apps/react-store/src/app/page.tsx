c44fa6a861f86cbd4dd1f62e76b2d86ba8405bd0
Trong Ngo <trong.ngotrung@gmail.com>
2025-04-25 10:48:33 +0700

    chore: set up libs/shared/core and libs/shared/utils

--- apps/react-store/src/app/page.tsx
+++ apps/react-store/src/app/page.tsx
@@ -1,4 +1,5 @@
-import environment from '@/environments';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import environment from '../../../../libs/shared/core/src/environments';
 
 export default function Index() {
   /*
